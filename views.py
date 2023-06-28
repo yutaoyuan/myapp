e00bb4a8ccf43409b054ef38c27e9c608d7ae82a
Joseph Pham <50855404+yutaoyuan@users.noreply.github.com>
2023-06-28 11:05:26 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
 
+def index(request):
+    return render(request, 'index.html')
+  
+def reputation_tags(request):
+    return render(request, 'reputation_tags.html')
+
+def reputation_log(request):
+    return render(request, 'reputation_log.html')
